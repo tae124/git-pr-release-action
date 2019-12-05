ce239036ff83f19efe460f97d16dbabab7b52eae
Tae Matsumoto <agnelloxy@gmail.com>
2019-12-06 02:27:13 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@
 set -eu
 export GIT_PR_RELEASE_TOKEN=$GITHUB_TOKEN
 git remote set-url origin https://$GITHUB_TOKEN:x-oauth-basic@github.com/$GITHUB_REPOSITORY.git
-git-pr-release
+git-pr-release --no-fetch
