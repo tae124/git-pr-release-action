43e82ba6fc123939668b66a635bf6da167b32953
agnelloxy <agnelloxy@gmail.com>
2019-12-25 23:39:26 +0900

    set tz

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,7 @@
 
 set -eu
 export GIT_PR_RELEASE_TOKEN=$GITHUB_TOKEN
+export TZ=$TZ
 git clone https://$GITHUB_TOKEN:x-oauth-basic@github.com/$GITHUB_REPOSITORY.git
 cd `echo ${GITHUB_REPOSITORY} | sed -n 's/^\(.*\)\/\(.*\)$/\2/p'`
 git remote set-url origin https://$GITHUB_TOKEN:x-oauth-basic@github.com/$GITHUB_REPOSITORY.git
